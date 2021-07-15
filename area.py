1ff8535c478da923dd41fc02fbce610ea74e83b2
YiSoo <underthes99@gmail.com>
2021-07-15 16:36:03 +0900

    git upload practice

---
+++ area.py
@@ -0,0 +1,17 @@
+#0715 codeit - 파이썬 심화
+# about Module
+PI=3.1415
+def circle(r):
+    """
+    원의 넓이 구하는 함수
+    r=pi*r**2
+    """
+    circleArea=PI*r*r
+    return circleArea
+
+def square(length):
+    """
+    정사각형 넓이 구하는 함수
+    """
+    squareArea=length*length
+    return squareArea
---
+++ run.py
@@ -0,0 +1,8 @@
+import area
+
+#모듈에 저장된 변수 불러올 때도 함수 불러오듯이 하면 됨
+print(area.PI)
+print(area.circle(5))
+print(area.square(10))
+
+
