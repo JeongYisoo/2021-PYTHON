6e29ebfbff4e98747dcf9a9fdbf595f133c49520
YiSoo <underthes99@gmail.com>
2021-08-19 10:33:26 +0900

    git upload practice

---
+++ codeup_1079.py
@@ -0,0 +1,17 @@
+# 첫번째 풀이
+# word=list(input().split())
+# find='q'
+#
+# for i in word:
+#     if i==find:
+#         print(i)
+#         break
+#     else:
+#         print(i)
+
+# 두번째 풀이
+while True:
+    word=input()
+    print(word)
+    if word == 'q':
+        break
